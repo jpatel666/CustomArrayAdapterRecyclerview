e4ac9cd603eb9481b34b85e2957d884739818afb
Jimmy <jhpatel392@gmail.com>
2023-10-12 23:15:12 -0700

    RecyclerView

--- app/src/main/java/com/example/customarrayadapterrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/customarrayadapterrecyclerview/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.customarrayadapterrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
@@ -19,6 +22,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView);
+
+        //Types:-LinearLayoutManager,GridLayoutManager,StaggeredGridLayoutManager       (Horizontal & Vertical Set Any Place)
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this,RecyclerView.HORIZONTAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+        //GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this,2,RecyclerView.HORIZONTAL,false );
+        //recyclerView.setLayoutManager(gridLayoutManager);
+
+
+        //Example Wallpaper App(StaggeredGridLayoutManager)
+        //StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
+        //recyclerView.setLayoutManager(staggeredGridLayoutManager);
+
+
 
         AppAdapter appAdapter = new AppAdapter(MainActivity.this,appname,appicon);  //Alt+Enter
         recyclerView.setAdapter(appAdapter);
